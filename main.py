1dfca78a12ebf7a76029b82d83709829a60c9397
Diga <dias.gainatulinsc@gmail.com>
2024-03-07 12:23:38 +0500

    A

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import process
 
 
 TOKEN = "6670441542:AAGnU4k3CGdvZ5klDM7a6jnb_w_iJZy2duc"
-ALLOWED_USERS = [801866076]
+ALLOWED_USERS = [801866076, 439638583]
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -62,6 +62,40 @@ def find_more_companies(update: Update, context: CallbackContext) -> None:
     else:
         update.message.reply_text('Вы не авторизованы для использования этого бота.')
 
+def start_find_by_country(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.from_user.id
+    if user_id in ALLOWED_USERS:
+        keyboard = [[InlineKeyboardButton("Найти компанию по стране", callback_data='find_by_country')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text("Введите страну:", reply_markup=reply_markup)
+    else:
+        update.message.reply_text('Вы не авторизованы для использования этого бота.')
+
+def handle_country_input(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.from_user.id
+    if user_id in ALLOWED_USERS:
+        global last_processed_excel_info
+        if last_processed_excel_info:
+            file_path = last_processed_excel_info["file_path"]
+
+            country = update.message.text
+
+            new_companies_info = process.process_input(file_path, country)
+
+            result_message = "\n\n".join(
+                [f"{i + 1}. {name}. {description}. Official site: {website}. Country: {country}" for i, (name, [description, website, country]) in enumerate(new_companies_info.items())])
+            update.message.reply_text(result_message)
+        else:
+            update.message.reply_text('Пожалуйста, сначала отправьте Excel файл для обработки.')
+    else:
+        update.message.reply_text('Вы не авторизованы для использования этого бота.')
+
+def button_callback(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    if query.data == 'find_by_country':
+        start_find_by_country(update, context)
+    else:
+        pass
 
 def main() -> None:
     os.makedirs("temp", exist_ok=True)
@@ -70,10 +104,12 @@ def main() -> None:
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
     dp.add_handler(MessageHandler(Filters.document.mime_type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"), handle_excel))
     dp.add_handler(CommandHandler("find", find_more_companies))
-
+    dp.add_handler(CommandHandler("find_by_country", start_find_by_country))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_country_input))
+    dp.add_handler(CallbackQueryHandler(button_callback))
+    dp.add_handler(CallbackQueryHandler(handle_country_input, pattern='find_by_country'))
 
     updater.start_polling()
 
--- process.py
+++ process.py
@@ -3,51 +3,99 @@ from openai import OpenAI
 
 client = OpenAI()
 
-def process_input(message):
+def checkExistence(companies, country):
+    new_companies = {}
+    for company_data in companies:
+        if company_data.strip():
+            company_info = company_data.split(' - ')
+            print("Company Info: ", company_info)
+            if len(company_info) == 2:
+                name = company_info[0].split('. ')[1].strip()
+                details = company_info[1].split('. ')
+                user_prompt = f"Do you know this company {name}? Answer in one word"
+                extraction_query = \
+                    client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": user_prompt}],
+                                                   max_tokens=300).choices[0].message.content
+                print("Query 1: ", extraction_query)
+                if (extraction_query == "Yes." or extraction_query == "Yes"):
+                    user_prompt = f"Is {name} company related to the medical field?? Answer in one word"
+                    extraction_query = \
+                        client.chat.completions.create(model="gpt-4",
+                                                       messages=[{"role": "user", "content": user_prompt}],
+                                                       max_tokens=300).choices[0].message.content
+                    print("Query 2: ", extraction_query)
+                    if (extraction_query == "Yes." or extraction_query == "Yes"):
+                        user_prompt = f"Is the {name} company based in {country}? Answer in one word"
+                        extraction_query = \
+                            client.chat.completions.create(model="gpt-4",
+                                                           messages=[{"role": "user", "content": user_prompt}],
+                                                           max_tokens=300).choices[0].message.content
+                        print("Query 3: ", extraction_query)
+                        if extraction_query == "Yes." or extraction_query == "Yes":
+                            if len(details) >= 3:
+                                description = details[0].strip()
+                                website = details[1].split('Website: ')[1].split('. ')[0].strip()
+                                country = details[2].split('Country: ')[1].strip()
+                                print("WebSite", description, website, country)
+                                new_companies[name] = [description, website, country]
+
+    return new_companies
+
+
+
+def process_input(file_path, country):
     try:
         with open("companies.txt", "r") as file:
             existing_companies = file.read().splitlines()
 
+        excel_file_path = file_path
+        df = pd.read_excel(excel_file_path)
+        companies = {}
+
+        n = len(df)
+        d = n
+        if (n > 20):
+            n = 20
+
+        company = []
+        for i in range(d):
+            company.append(df.loc[i, 'Distributor Name'])
+        for i in range(n):
+            companies[df.loc[i, 'Distributor Name']] = df.loc[i, 'Description']
+
+
+        print(companies)
         example = "1. 'Company Name' - 'Description Of Company'. Website: https://www.example.com. Country: United States"
 
-        user_prompt = f"{message}. Also provide an official site of these new companies and the country of foundation. Here is an example of your output: {example}. Just don't wrap your answer in extra quotes. You need to find companies that are not here: {existing_companies}. Make the description in one sentence. Replace all periods in the description sentence with a comma, not counting the end of the sentence. Display information about each company separating them '\\n\\n'."
+        companies_result = {}
 
-        extraction_query = \
-        client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": user_prompt}],
-                                       max_tokens=300).choices[0].message.content
-        print("I am", extraction_query)
+        user_prompt = f"You have {companies} and {companies_result}. It is a companies with description. You need to find 5 new companies similar to the ones I provided. Also provide a official site of these new companies and country of foundation. Here an example of your output: {example}. Just don't wrap your answer in extra quotes. You need to find companies that are not here: {existing_companies}. And don’t think that you can provide a company that is on this list simply by changing the company name. Make description in one sentence. Replace all periods in the description sentence with a comma, not counting the end of the sentence. Display information about each company separating them '\\n\\n'. If you don’t know more companies, don’t lie. No need to invent companies. You need to find companies from {country}. And again, please don’t inflate companies and their descriptions."
 
-        companies_data = extraction_query.split('\n\n')
-        print("Companies Data: ", companies_data)
 
-        new_companies = {}
-        for company_data in companies_data:
-            print(company_data)
-            if company_data.strip():
-                # Splitting company info into parts
-                company_info = company_data.split(' - ')
-                print("Company Info: ", company_info)
-                if len(company_info) == 2:
-                    name = company_info[0].split('. ')[1].strip()
-                    details = company_info[1].split('. ')
-                    print("Name", name, details)
+        while True:
+            extraction_query = \
+            client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": user_prompt}],
+                                           max_tokens=300).choices[0].message.content
 
-                    if len(details) >= 3:
-                        description = details[0].strip()
-                        website = details[1].split('Website: ')[1].split('. ')[0].strip()
-                        country = details[2].split('Country: ')[1].strip()
-                        print("WebSite", description, website, country)
-                        new_companies[name] = [description, website, country]
+            print("I am", extraction_query)
 
-        print(new_companies)
+            companies_data = extraction_query.split('\n\n')
+            print("Companies Data: ", companies_data)
+            quantity = checkExistence(companies_data, country)
+            companies_result.update(quantity)
+            print("Companies result: ", companies_result)
+            if len(companies_result) >= 5:
+                break
+
+
+        print(companies_result)
 
         with open("companies.txt", "a+", encoding="utf-8") as file:
-            for comp, _ in new_companies.items():
+            for comp, _ in companies_result.items():
                 if comp not in existing_companies:
                     file.write(comp + "\n")
 
-        return new_companies
-
+        return companies_result
     except Exception as e:
         error_message = f"An error occurred: {str(e)}"
         print(error_message)
@@ -86,7 +134,7 @@ def process_excel(file_path):
         print(companies)
         example = "1. 'Company Name' - 'Description Of Company'. Website: https://www.example.com. Country: United States"
 
-        user_prompt = f"You have {companies}. It is a companies with description. You need to find 5 new companies similar to the ones I provided. Also provide a official site of these new companies and country of foundation. Here an example of your output: {example}. Just don't wrap your answer in extra quotes. You need to find companies that are not here: {existing_companies}. Make description in one sentence. Replace all periods in the description sentence with a comma, not counting the end of the sentence. Display information about each company separating them '\\n\\n'."
+        user_prompt = f"You have {companies}. It is a companies with description. You need to find 5 new companies similar to the ones I provided. Also provide a official site of these new companies and country of foundation. Here an example of your output: {example}. Just don't wrap your answer in extra quotes. You need to find companies that are not here: {existing_companies}. And don’t think that you can provide a company that is on this list simply by changing the company name. Make description in one sentence. Replace all periods in the description sentence with a comma, not counting the end of the sentence. Display information about each company separating them '\\n\\n'. If you don’t know more companies, don’t lie. No need to invent companies."
 
         extraction_query = \
         client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": user_prompt}],
